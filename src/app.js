9a2f40e2067d8630b62a3331d558af04fd9e24ca
Alex Bannow <rbannal@gmail.com>
2020-01-02 16:01:58 -0500

    deployment setup

--- src/app.js
+++ src/app.js
@@ -13,7 +13,9 @@ const app = express()
 app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
   skip: () => NODE_ENV === 'test',
 }))
-app.use(cors())
+app.use(cors({
+  origin: CLIENT_ORIGIN
+}))
 app.use(helmet())
 
 app.use('/api/auth', authRouter)
